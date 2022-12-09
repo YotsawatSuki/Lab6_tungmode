13a9007f0eac120339004d4bce6e683bca681ab9
Yotsawat Sukinee_ <yotsawat2557@gmail.com>
2022-12-09 15:28:03 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
+    int x;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+
+    if (x!=0){
+       int y = 0;
+       int z = 0;
+       while (x>0){
+            if(x%2 == 0){
+                y++;
+            }else{
+                z++;
+            }
+            cout << "Enter an integer: ";
+            cin >> x;
+            
+        }
+        cout << "#Even numbers = " << y <<"\n";
+        cout << "#Odd numbers = " << z;
+
+    }else{
+        cout << "#Even numbers = " << x <<"\n";
+        cout << "#Odd numbers = " << x;
+    }
+    
     return 0;
 }
